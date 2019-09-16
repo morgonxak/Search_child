47a2162d0211c5f61a15e5e35cac488447a3e872
cmitd <cmit.dima@gmail.com>
2019-09-16 17:25:04 +0700

    first commit

---
+++ app/src/main/java/com/kotlincodes/locationlistenerwithkotlin/main_activity_parent.kt
@@ -0,0 +1,12 @@
+package com.kotlincodes.locationlistenerwithkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class main_activity_parent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_parent)
+    }
+}
